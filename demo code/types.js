ce892cfa4029e3487a969367da42359a575657e0
qqli1209 <lqq201194@163.com>
2017-11-29 11:04:36 +0800

    finish Chapter Types

---
+++ demo code/types.js
@@ -0,0 +1,29 @@
+/**
+ * @copyright Qianqian Li, 2017
+ * @file types.js
+ */
+
+var a = Symbol("a");
+var b = Symbol("a");
+console.log(typeof a); // symbol
+console.log(a == b); // false
+console.log(typeof null); // object
+
+function c(){}
+console.log(typeof c); // function
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
